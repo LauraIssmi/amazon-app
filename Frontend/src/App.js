33ff72af5ebcd64a5da6d76da03b91fae8b633d2
BENKHEDIR Maroua <maroua2012@gmail.com>
2020-12-27 19:04:25 +0100

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+function App() {
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="row">
+					<div>
+						<a href="/" className="brand">
+							amazon
+						</a>
+					</div>
+					<div>
+						<a href="/cart">Cart</a>
+						<a href="/signin">Sign In</a>
+					</div>
+				</header>
+				<main>
+					<Route path="/product/:id" component={ProductScreen}></Route>
+					<Route path="/" exact component={HomeScreen}></Route>
+				</main>
+				<footer className="row center">All right are reserved</footer>
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;
