fea1c937d7de6b10c84dabff42afc706295f1018
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-04 16:19:36 +0100

    Create Product

--- Frontend/src/actions/productActions.js
+++ Frontend/src/actions/productActions.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/ProductConstants";
+import { PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/ProductConstants";
 
 export const listProducts = () => async (dispatch) => {
 	dispatch({
@@ -23,5 +23,17 @@ export const detailsProduct = (productId) => async (dispatch) => {
 		dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.message : error.message });
+	}
+};
+export const createProduct = () => async (dispatch, getState) => {
+	dispatch({ type: PRODUCT_CREATE_REQUEST });
+	const {
+		userSignin: { userInfo },
+	} = getState();
+	try {
+		const { data } = await Axios.post("/api/products", {}, { headers: { Authorization: `Bearer ${userInfo.token}` } });
+		dispatch({ type: PRODUCT_CREATE_SUCCESS, payload: data.product });
+	} catch (error) {
+		dispatch({ type: PRODUCT_CREATE_FAIL, payload: error.response && error.response.data.message ? error.response.message : error.message });
 	}
 };
