19bc61d43c8704d564dbdc0ed214db7cfa1ac34e
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-06 17:03:01 +0100

    44. Deliver Order

--- Frontend/src/Screens/OrderScreen.js
+++ Frontend/src/Screens/OrderScreen.js
@@ -4,17 +4,22 @@ import { Link } from "react-router-dom";
 import { PayPalButton } from "react-paypal-button-v2";
 import MessageBox from "../components/MessageBox";
 import LoadingBox from "../components/LoadingBox";
-import { detailsOrder, payOrder } from "../actions/orderActions";
+import { deliverOrder, detailsOrder, payOrder } from "../actions/orderActions";
 import axios from "axios";
-import { ORDER_PAY_RESET } from "../constants/orderConstants";
+import { ORDER_DELIVER_RESET, ORDER_PAY_RESET } from "../constants/orderConstants";
 
 export default function OrderScreen(props) {
 	const orderId = props.match.params.id;
 	const [sdkReady, setSdkReady] = useState(false);
 	const orderDetails = useSelector((state) => state.orderDetails);
 	const { order, loading, error } = orderDetails;
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
 	const orderPay = useSelector((state) => state.orderPay);
 	const { loading: loadingPay, error: errorPay, success: successPay } = orderPay;
+	const orderDeliver = useSelector((state) => state.orderDeliver);
+	const { loading: loadingDeliver, error: errorDeliver, success: successDeliver } = orderDeliver;
+
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -29,8 +34,9 @@ export default function OrderScreen(props) {
 			};
 			document.body.appendChild(script);
 		};
-		if (!order || successPay || (order && order._id !== orderId)) {
+		if (!order || successPay || successDeliver || (order && order._id !== orderId)) {
 			dispatch({ type: ORDER_PAY_RESET });
+			dispatch({ type: ORDER_DELIVER_RESET });
 			dispatch(detailsOrder(orderId));
 		} else {
 			if (!order.isPaid) {
@@ -41,12 +47,14 @@ export default function OrderScreen(props) {
 				}
 			}
 		}
-	}, [dispatch, order, orderId, sdkReady, successPay]);
+	}, [dispatch, order, orderId, sdkReady, successPay, successDeliver]);
 	const successPaymentHandler = (paymentResult) => {
 		//todo dispatch pay order
 		dispatch(payOrder(order, paymentResult));
 	};
-
+	const deliverHandler = () => {
+		dispatch(deliverOrder(order._id));
+	};
 	return loading ? (
 		<LoadingBox></LoadingBox>
 	) : error ? (
@@ -154,6 +162,15 @@ export default function OrderScreen(props) {
 											<PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler}></PayPalButton>
 										</>
 									)}
+								</li>
+							)}
+							{userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+								<li>
+									{loadingDeliver && <LoadingBox></LoadingBox>}
+									{errorDeliver && <MessageBox variant="danger">{errorDeliver}</MessageBox>}
+									<button type="button" className="primary block" onClick={deliverHandler}>
+										Deliver Order
+									</button>
 								</li>
 							)}
 						</ul>
