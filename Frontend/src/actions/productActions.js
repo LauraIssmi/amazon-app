34df5fd950fab374a1c991c7d56ee19fc4b74d8d
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-10 19:28:25 +0100

    54. Complete Advanced Search

--- Frontend/src/actions/productActions.js
+++ Frontend/src/actions/productActions.js
@@ -2,12 +2,12 @@ import Axios from "axios";
 
 import { PRODUCT_CATEGORY_LIST_FAIL, PRODUCT_CATEGORY_LIST_REQUEST, PRODUCT_CATEGORY_LIST_SUCCESS, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS } from "../constants/ProductConstants";
 
-export const listProducts = ({ seller = "", name = "", category = "" }) => async (dispatch) => {
+export const listProducts = ({ seller = "", name = "", category = "", order = "", min = 0, max = 0, rating = 0 }) => async (dispatch) => {
 	dispatch({
 		type: PRODUCT_LIST_REQUEST,
 	});
 	try {
-		const { data } = await Axios.get(`/api/products?seller=${seller}&name=${name}&category=${category}`);
+		const { data } = await Axios.get(`/api/products?seller=${seller}&name=${name}&category=${category}&min=${min}&max=${max}&rating=${rating}&order=${order}`);
 		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
