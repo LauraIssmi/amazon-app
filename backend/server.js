1d0ff68141a81b777c4f21fce507f640c4083ba0
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-11 14:53:04 +0100

    Frontend error

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 //app.get(`/`, (req, res) => {
 //	res.send("server is ready");
 //});
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/Frontend/build")));
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/Frontend/build/index.html")));
 //erreur catcher
 app.use((err, req, res, next) => {
